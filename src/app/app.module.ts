d5ad56788e2f484f6385faf771b9eb81bf6ebad5
Bugaienko <sergiibugaienko@gmail.com>
2023-06-20 21:56:36 +0200

    table component IW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CategoriesComponent } from './views/categories/categories.component';
 import { TaskListComponent } from './views/task-list/task-list.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
    declarations: [
@@ -14,7 +17,10 @@ import { TaskListComponent } from './views/task-list/task-list.component';
    ],
    imports: [
       BrowserModule,
-      BrowserAnimationsModule
+      BrowserAnimationsModule,
+      MatTableModule,
+      MatSortModule,
+      MatPaginatorModule
    ],
    providers: [],
    bootstrap: [AppComponent]
--- src/app/views/task-list/task-list.component.ts
+++ src/app/views/task-list/task-list.component.ts
@@ -1,23 +1,51 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from "../../model/Task";
 import {DataHandlerService} from "../../service/data-handler.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
-  selector: 'app-task-list',
-  templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+   selector: 'app-task-list',
+   templateUrl: './task-list.component.html',
+   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent implements OnInit{
+export class TaskListComponent implements OnInit {
+
+   displayedColumns: string[] =
+      ['color', 'id', 'title', 'date', 'priority', 'category'];
+   dataSource!: MatTableDataSource<Task>;
 
    tasks?: Task[];
+
    constructor(private dataHandler: DataHandlerService) {
    }
+
    ngOnInit(): void {
-      this.dataHandler.taskSubject.subscribe(tasks => this.tasks=tasks);
+      this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+
+      this.dataSource = new MatTableDataSource();
+
+      this.refreshTable();
+
    }
 
 
    toggleTaskCompleted(task: Task) {
       task.completed = !task.completed;
+   }
+
+   getPriorityColor(task: Task): string {
+      if (task.completed) {
+         return '#0094FD26';
+      }
+
+      if (task.priority && task.priority.color) {
+         return task.priority.color;
+      }
+      return '#fff';
+   }
+
+   private refreshTable() {
+      // @ts-ignore
+      this.dataSource.data = this.tasks;
    }
 }
